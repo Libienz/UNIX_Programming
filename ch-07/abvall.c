ea7719cda7ead8532a1137ec71442508ad72af75
Libienz <Leekh10195@gmail.com>
2022-11-08 22:53:40 +0900

    ch-07 done

--- ch-07/abvall.c
+++ ch-07/abvall.c
@@ -5,27 +5,27 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define BUFSIZE  4096 //... 좀 그러네
+#define BUFSIZE  4096 
 
 int main(void) {
 
-    char buf[BUFSIZE];
+    char buf[BUFSIZE]; //read할때 사용될 버퍼
     pid_t pid;
     FILE* wfp;
-    int rfd;
-    int n;
-    int ch;
+    int rfd; 
+    int n; //저수준에서 읽은 바이트수를 담을 변수
+    int ch;//getchar로 담아온 char 담을 변수 
     int wstatus;
     
 
-    if ((pid=fork()) < 0) {
+    if ((pid=fork()) < 0) { // fork 에러 
 	perror("fork");
 	exit(1);
     }
 
 
     
-    if (pid == 0) { //자식 프로세스
+    else if (pid == 0) { //자식 프로세스
         wfp = fopen("data.txt", "w");
 	while((ch=getchar())!= EOF) {
 	    putc(ch,wfp);
@@ -39,8 +39,10 @@ int main(void) {
 	rfd = open("data.txt", O_RDONLY);
 	wait(&wstatus);
 	while ((n = read(rfd, buf, BUFSIZE-1)) > 0) {
+	    buf[BUFSIZE] = '\0';
+	    printf("%s",buf);
 
-	    if (write(1, buf,BUFSIZE) != n) perror("Write");
+	   //if (write(1, buf,BUFSIZE) != n) perror("Write");
 	}
 	close(rfd);
     }
