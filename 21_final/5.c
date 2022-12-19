779ab530f9115446f0bff34dab038b8cf8509401
Libienz <Leekh10195@gmail.com>
2022-12-19 17:36:39 +0900

    21 final done

---
+++ 21_final/5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//SIGINT 처리하는데 사용될 handler 아무것도 하지 않는다.
+void sig_handler(int signo) {
+    //do nothing
+}
+
+int main(void) {
+    struct sigaction act;
+
+    //블로킹 따로 지정 안함
+    //sa_flag가 0임으로 시그널 핸들러를 호출되게 하는 시그널만 블로킹
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    act.sa_handler = sig_handler;
+
+    //시그널 핸들러를 지정하며 여러 처리과정까지 제어할 수 있는 sigaction
+    //SIGINT를 처리하는 핸들러와 flag, mask가 지정된 구조체를 인자로 넘긴다.
+    if (sigaction(SIGINT, &act, (struct sigaction *)NULL) < 0) {
+	perror("sigaction");
+	exit(1);
+    }
+
+    short i = 0;
+    while (1) {
+	if (i<0)
+	    i = 0;
+	printf("%d\r", i++);
+    }
+    return 0;
+}
+
